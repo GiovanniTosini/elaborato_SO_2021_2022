a30d310ebd3cb728e6b4df963b3febd93a472c15
Rumpappa <poppo@pop-os.localdomain>
2022-04-29 16:16:57 +0200

    settato i segnali di client_0

--- client_0.c
+++ client_0.c
@@ -1,9 +1,75 @@
 /// @file client.c
 /// @brief Contiene l'implementazione del client.
 
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 #include "defines.h"
+#include "err_exit.h"
+
+void sigHandler(int sig) {
+    if(sig == SIGUSR1){
+        printf("Termination of Client_0 process.\n");
+        if(kill(getpid(), SIGTERM) == -1){
+            ErrExit("Failed to terminate client_0");
+        }
+    }
+    else if(sig == SIGINT) {
+        printf("Client_0 waiting...\n");
+    }
+}
 
 int main(int argc, char * argv[]) {
+
+    //creazione, inizializzazione e gestione dei segnali
+    sigset_t signalSet;
+    //rimozione di tutti i segnali
+
+    //variante
+    /*if(sigfillset(&signalSet) || sigdelset(&signalSet, SIGINT | SIGUSR1) ||
+            sigprocmask(SIG_SETMASK, &signalSet, NULL) == -1){
+        ErrExit("Something wrong I can smell it");
+    }*/
+
+    if(sigfillset(&signalSet) == -1){
+        ErrExit("Failed to fill the signals");
+    }
+    //aggiunta dei segnali SIGINT e SIGUSR1 alla maschera
+    if(sigdelset(&signalSet, SIGINT | SIGUSR1) == -1){
+        ErrExit("Failed to set the signals");
+    }
+    if(sigprocmask(SIG_SETMASK, &signalSet, NULL) == -1){
+        ErrExit("Failed to set the signal mask");
+    }
+
+    //settaggio del sigHandler
+    if(signal(SIGUSR1 | SIGINT, sigHandler) == SIG_ERR){
+        ErrExit("Signal handler set failed");
+    }
+    //messo in attesa dei segnali desiderati
+    while(1){
+        pause();
+        //settaggio maschera
+        if(sigaddset(&signalSet, SIGINT | SIGUSR1) == -1){
+            ErrExit("Failed to reset the signals");
+        }
+        //impostazione della directory
+        if(chdir(argv[1]) == -1){
+
+        }
+    }
+
+
+
+
+
+
+
 
     return 0;
 }
