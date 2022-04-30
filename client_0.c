1e22d616d1c35bae25ab8cb03b42183f7fe3a943
Alessandro <alessandro@pop-os.localdomain>
2022-04-30 08:06:32 +0200

    inizio controllo nome file

--- client_0.c
+++ client_0.c
@@ -8,9 +8,13 @@
 #include <signal.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <dirent.h>
 
 #include "defines.h"
 #include "err_exit.h"
+#define BUFFER_SZ 150
+
+char currdir[BUFFER_SZ];
 
 void sigHandler(int sig) {
     if(sig == SIGUSR1){
@@ -24,10 +28,27 @@ void sigHandler(int sig) {
     }
 }
 
+size_t append2Path(char *directory){
+    size_t lastPathEnd=strlen(currdir);
+    strcat(strcat(&currdir[lastPathEnd],"/"),directory);
+    return lastPathEnd;
+}
+
+int checkFileName(char *filename,char *string){
+    //controllo nome file
+
+        //1: ok
+        //0: no
+    }
+
+}
+
 int main(int argc, char * argv[]) {
 
     //creazione, inizializzazione e gestione dei segnali
     sigset_t signalSet;
+    char *user;
+    
     //rimozione di tutti i segnali
 
     //variante
@@ -60,13 +81,35 @@ int main(int argc, char * argv[]) {
         }
         //impostazione della directory
         if(chdir(argv[1]) == -1){
+            ErrExit("Failed to switch directory");
+        }
+
+        user=getenv("USER");
+        if(getcwd(currdir,sizeof(currdir))==NULL){
+            ErrExit("getcwd failed!");
+        }
+        
+        printf("Ciao %s, ora inizio l’invio dei file contenuti in %s",user,currdir);
+        DIR *dirp=opendir(currdir);
+        
+        struct dirent *dentry;
+
+        while((dentry=readdir(dirp))!=NULL){
+            if(strcmp(dentry->d_name,".")==0||strcmp(dentry->d_name,".."==0)){
+                continue;
+            }
+            if(dentry->d_type==DT_REG){
+                size_t lastPath=append2Path(dentry->d_name); 
+
+                int match=checkFileName(dentry->d_name, dentry->d_name + "/sendme_");
+
+                if(match==1) //true
+
+            }
+
+
 
         }
-    }
-
-
-
-
 
 
 
