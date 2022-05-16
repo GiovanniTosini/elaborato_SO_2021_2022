4601893ea82e4c8f54f69ad0c4e204fcaed8216b
Alessandro <alessandro@pop-os.localdomain>
2022-05-16 14:24:43 +0200

    implementato il semaforo per far partire i client tutti assieme

--- client_0.c
+++ client_0.c
@@ -12,9 +12,11 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/shm.h>
+#include <sys/sem.h>
 
 #include "defines.h"
 #include "err_exit.h"
+#include "semaphore.h"
 
 #define BUFFER_SZ 150
 #define N_FILES 100
@@ -24,12 +26,13 @@ char *files[N_FILES]; //pathname file
 int n_files = 0;
 char *fifo1name = "/tmp/myfifo1";
 char *fifoShmId = "/tmp/myFifoForId";
+int attesa=0;
 
 void sigHandler(int sig) {
     if(sig == SIGUSR1){
         printf("Termination of Client_0 process.\n");
         if(kill(getpid(), SIGTERM) == -1){
-            ErrExit("Failed to terminate client_0");
+            errExit("Failed to terminate client_0");
         }
     }
     else if(sig == SIGINT) {
@@ -60,40 +63,40 @@ int main(int argc, char * argv[]) {
     //variante
     /*if(sigfillset(&signalSet) || sigdelset(&signalSet, SIGINT | SIGUSR1) ||
             sigprocmask(SIG_SETMASK, &signalSet, NULL) == -1){
-        ErrExit("Something wrong I can smell it");
+        errExit("Something wrong I can smell it");
     }*/
 
     //riempio la signalSet con tutti i segnali
     if(sigfillset(&signalSet) == -1){
-        ErrExit("Failed to fill the signals");
+        errExit("Failed to fill the signals");
     }
     //aggiunta dei segnali SIGINT e SIGUSR1 alla maschera
     if(sigdelset(&signalSet, SIGINT | SIGUSR1) == -1){
-        ErrExit("Failed to set the signals");
+        errExit("Failed to set the signals");
     }
     if(sigprocmask(SIG_SETMASK, &signalSet, NULL) == -1){
-        ErrExit("Failed to set the signal mask");
+        errExit("Failed to set the signal mask");
     }
 
     //settaggio dei segnali al sigHandler
     if(signal(SIGUSR1 | SIGINT, sigHandler) == SIG_ERR){
-        ErrExit("Signal handler set failed");
+        errExit("Signal handler set failed");
     }
     //in attesa dei segnali desiderati
     while(1){
         pause();
         //settaggio maschera
         if(sigaddset(&signalSet, SIGINT | SIGUSR1) == -1){
-            ErrExit("Failed to reset the signals");
+            errExit("Failed to reset the signals");
         }
         //impostazione della directory
         if(chdir(argv[1]) == -1){
-            ErrExit("Failed to switch directory");
+            errExit("Failed to switch directory");
         }
 
         //otteniamo la current working directory
         if(getcwd(currdir,sizeof(currdir)) == NULL){
-            ErrExit("getcwd failed!");
+            errExit("getcwd failed!");
         }
         
         printf("Ciao %s, ora inizio l’invio dei file contenuti in %s", getenv("USER"), currdir);
@@ -114,12 +117,25 @@ int main(int argc, char * argv[]) {
         //attende di ricevere messaggio da shared memory
         char *shmPointer = (char *) shmat(shmId, NULL, 0);
         while(shmPointer != "confirmed");
+
+        //creazione del semaforo che verrà usato dai figli
+        int semid=semget(IPC_PRIVATE,1,S_IRUSR|S_IWUSR);
+        if(semid==-1)
+            errExit("semget failed");
+
+        //inizializzazione del semaforo
+        union semun arg;
+        arg.val=n_files; //set del valore iniziale
+        if(semctl(semid,0,SETALL,arg)==-1){
+            errExit("semctl set failed!");
+        }
+
         //generazione figli
         pid_t pid;
-        for(int i = 0; i < n_files; i++){
+        for(int child = 0; child < n_files; child++){
             pid=fork();
             if(pid == -1)
-                ErrExit("fork error");
+                errExit("fork error");
             else if(pid == 0){
                 struct stat sb;
                 char *buff;
@@ -129,27 +145,29 @@ int main(int argc, char * argv[]) {
                 char *sendByShM; //variabile in cui salvare per shm
                 int fd, counter = 0, incremento = 0, totalByte = 0, porzione = 0, bloccoByte = 0;
 
-                fd = open(files[i], O_RDONLY, S_IRUSR);
-                stat(files[i], &sb);
-                read(fd, buff, sb.st_size);
-                incremento = divideBy4((sb.st_size)/8); //incremento necessario per dividere per 4
+                fd = open(files[child], O_RDONLY, S_IRUSR); //apertura child-esimo file
+                stat(files[child], &sb); //prendo statistiche file
+                read(fd, buff, sb.st_size); //leggo il file
+
+                divideString(buff,sendByFIFO1,sendByFIFO2,sendByMsgQ,sendByShM); //dividiamo il file e lo salviamo nelle stringhe
 
 
-
-                porzione = (counter + incremento) / 4; //un quarto dei caratteri
-                bloccoByte = porzione * 8; //singolo blocco di byte
-                totalByte = (incremento + counter) * 8; //byte totali da leggere
-
-
+                //blocco il figlio
+                attesa++; //controllo attesa figli
+                semOp(semid, (unsigned short)0, -1); //TODO: Da verificare!
+                semOp(semid, (unsigned short)0, 0); //Rimane fermo fin quando tutti non sono 0.
+                //iniziano inviare
 
                 
             }else{
                 //codice padre client_0
+                //attesa msg queue
+
             }
         }
 
         if(pid == -1)
-            ErrExit("fork error!");
+            errExit("fork error!");
         if(pid == 0)
             //figlio che fa cose...
 	
--- semaphore.h
+++ semaphore.h
@@ -3,3 +3,11 @@
 ///         specifiche per la gestione dei SEMAFORI.
 
 #pragma once
+
+union semun{
+    int val;
+    struct semid_ds;
+    unsigned short array;
+};
+
+void semOp(int semid,unsigned short sem_num, short sem_op);
